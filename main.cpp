b5f79215ded93a11a33cb546f3163c4837d4f870
Fnav05 <felvinnavarro10@gmail.com>
2026-03-23 18:29:31 -0700

    Pytest 2 fail?

--- main.cpp
+++ main.cpp
@@ -6,6 +6,23 @@ int main()
 {
     int i, begin, end, sum = 0;
 
-    // TODO: prompt user for begin and end values
+    // TODO: prompt user for begin and end valueswhile (1) {
+    while (1) {
+    cout << "Enter being and end: ";
+    cin >> begin >> end;
+    if (begin <= end) {
+      break;
+    }
+}
     // TODO: loop from begin to end and accumulate sum of even values
+    if (begin % 2 == 1) {
+      begin++;
+    }
+    i = begin;
+    
+    while (i <= end) {
+      sum += i;
+      i += 2;
+    }
+    cout << "The sum of even values is " << sum << endl;
 }
